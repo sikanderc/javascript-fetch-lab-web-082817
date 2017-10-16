f3820ee862c1a70fcb151480539e07ee8b17b24c
sikanderc <sikanderchowhan@gmail.com>
2017-10-16 14:19:09 -0400

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
 function getIssues() {
+  fetch('https://api.github.com/repos/sikanderc/javascript-fetch-lab/issues', {
+    method: /get/
+  }).then(res => res.json()).then(json => console.log(json));
+
 }
 
 function showIssues(json) {
 }
 
 function createIssue() {
+  fetch('https://api.github.com/repos/sikanderc/javascript-fetch-lab/issues', {
+    method: /post/,
+    headers: {
+      Authorization: `token ${token}`
+    },
+    body: {
+      title: "pretty pretty pretty rickay"
+    }
+  }).then(res => res.json()).then(json => console.log(json));
+
 }
 
 function showResults(json) {
 }
 
 function forkRepo() {
-  const repo = 'learn-co-curriculum/javascript-fetch-lab'
   //use fetch to fork it!
+  fetch('https://api.github.com/repos/learn-co-curriculum/javascript-fetch-lab/forks', {
+    method: /post/,
+    headers: {
+      Authorization: `token ${token}`
+    }
+  }).then(res => res.json()).then(json => console.log(json));
+
 }
 
 function getToken() {
@@ -20,3 +40,5 @@ function getToken() {
   //back to '' before committing so all tests pass
   return ''
 }
+
+const token = '704114db17314e28baf3d509db3db9e00760db2d';
